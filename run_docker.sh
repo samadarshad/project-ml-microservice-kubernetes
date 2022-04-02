f948cccd92178ebaa93dc634cbf0bb8fe92be48c
Samad Arshad <samad.arshad@ev.energy>
2022-04-02 13:15:49 +0100

    Working app host

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build . --tag=sklearn-predictor
 
 # Step 2: 
 # List docker images
+docker image ls
 
 # Step 3: 
 # Run flask app
+docker container run -p 8000:80 sklearn-predictor
